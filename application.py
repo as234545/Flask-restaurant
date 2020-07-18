bfb6712bed17cfe2deaa1cbad19f3c480703999c
Halah Alabbad <38733409+as234545@users.noreply.github.com>
2020-07-19 01:07:45 +0300

    remove test statement and change date parse

--- application.py
+++ application.py
@@ -10,10 +10,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from keystoneclient.session import Session
 import sqlite3
 from datetime import datetime, timedelta, time
-#engine = create_engine('sqlite:///db6.sqlite', echo=True)
 
-
-#app._static_folder = "C:/Users/hh577/RestTest/static"
 
 def get_db_connection():
     conn = sqlite3.connect('db6.db')
@@ -51,32 +48,14 @@ def signup_post():
     name = request.form.get('name')
     password = request.form.get('password')
 
-    #Session = sessionmaker(bind=engine)
-    #s = Session()
-
-    #query = s.query(User).filter(User.email.in_([email]) )
-    #result = query.one_or_none()
-    #if result is not None:
-     #   flash("Email address already exists")
-     #   return redirect(url_for('signup'))
-    print("111111")
     user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
-    print("222222")
+
     if user: # if a user is found, we want to redirect back to signup page so user can try again
         flash("Thie user already exists")
         return redirect(url_for('signup'))
-    print("3333333")
-
-
-
+    
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
     new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
-    print("4444444")
-    # add the new user to the database
-
-    #s.add(new_user)
-    #s.commit()
-    # add the new user to the database
     db.session.add(new_user)
     db.session.commit()
 
@@ -88,24 +67,15 @@ def login_post():
     password = request.form.get('password')
     print(password)
     remember = True if request.form.get('remember') else False
-    #Session = sessionmaker(bind=engine)
-    #s = Session()
-    #user = s.query(User).filter(User.email.in_([email]) )
-    #user = User.query.filter_by(email=email).first()
-    print("ggg")
     user = User.query.filter_by(email=email).first()
-    #print(user)
-    # check if the user actually exists
-    # take the user-supplied password, hash it, and compare it to the hashed password in the database
-    print("rrrrrrr")
+
     print(user)
     if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
-        print("nononon")
         return redirect(url_for('login')) # if the user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
-    print("yes")
+
     login_user(user, remember=remember)
     return redirect(url_for('index'))
 
@@ -121,17 +91,11 @@ def Reservation1():
     es = Resturants.query.all()
     guestNum = int(request.args.get('num'))
     date = request.args.get("date")
-    date3 = datetime.strptime(date, "%Y-%m-%d")
+    date = date.replace('T', '') 
+    date3 = datetime.strptime(date, "%Y-%m-%d%H:%M")
     textarea = request.args.get('textarea')
     res_id = request.args.get('res_id')
     opstion = request.args.get('comp_select')
-    print("gN")
-    print(guestNum)
-    print(type(guestNum))
-    print(date)
-    print(date3)
-    print(textarea)
-    int(res_id)
     if date3 < datetime.now():
         flash("please choose a future date ")
         return redirect(url_for('Reservation2' ,res_id = res_id ))
